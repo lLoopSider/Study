c6b8e82c7063451d25109e00844553858279b212
LoopCider <liuguangxuan2003@hotmail.com>
2021-01-22 17:34:48 +0800

    提交知识点并对数组做修改

--- Hello.java
+++ Hello.java
@@ -2,9 +2,8 @@ public class Hello {
     public static void main(String[] arges) {
         int a=111;
         System.out.println(a);
-        int[] b=new int[5];
-        b[0]=1111;
-        b[2]=9999;
+        int[] b;
+        b=new int[] {11,22,33,44,55};
         System.out.println(b[2]);
         System.out.println(b.length);
     }
