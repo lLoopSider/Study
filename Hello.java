ffda4e1b01ece9abd2212b5e1ad56ee893b8fe75
LoopCider <liuguangxuan2003@hotmail.com>
2021-01-22 17:28:13 +0800

    数组

--- Hello.java
+++ Hello.java
@@ -2,5 +2,10 @@ public class Hello {
     public static void main(String[] arges) {
         int a=111;
         System.out.println(a);
+        int[] b=new int[5];
+        b[0]=1111;
+        b[2]=9999;
+        System.out.println(b[2]);
+        System.out.println(b.length);
     }
 }
