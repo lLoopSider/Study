dfd75d38d3e84f167c04916efcda84a5469d1210
LoopCider <liuguangxuan2003@hotmail.com>
2021-01-22 19:19:30 +0800

    学习了输入的方法

--- Hello.java
+++ Hello.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+import Java.util.Scanner;
 public class Hello {
     public static void main(String[] arges) {
         int a=111;
@@ -6,5 +9,11 @@ public class Hello {
         b=new int[] {11,22,33,44,55};
         System.out.println(b[2]);
         System.out.println(b.length);
+        Scanner c= new Scanner(System.in);
+        System.out.println("please enter a string and a number:");
+        String name=c.nextLine();
+        int num=c.nextInt();
+        System.out.printf("Hi!, %s,number is %d",name,num);
+
     }
 }
