01e4cfa504534da89da1bc1c690caf3c6d903d08
LoopCider <liuguangxuan2003@hotmail.com>
2021-01-23 09:33:14 +0800

    game加入

---
+++ Python/pygame/settings.py
@@ -0,0 +1,14 @@
+#设置
+class Settings():
+    def __init__(self):
+        self.screen_width=1200
+        self.screen_height=800
+        self.bg_color=(230,230,230)
+        
+        self.ship_speed_factor=2
+        
+        self.bullet_speed_factor=3
+        self.bullet_width=3
+        self.bullet_height=10
+        self.bullet_color=(0,16,225)
+        self.bullets_allowed=4
